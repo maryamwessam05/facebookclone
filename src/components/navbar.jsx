6739b5bc8081c8f1acea7deed24937a8fb424a36
maryamwessam05 <23-101015@students.eui.edu.eg>
2026-02-11 11:39:00 +0200

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './navbar.css'
+import logoimg from "../assets/logo.svg"
+import navicon from "../assets/icon.svg"
+import profileimg from "../assets/profile.svg"
 
 const Navbar = () => {
     return ( 
         <>
         <div className="nav">
-            
+            <img src={logoimg} alt="logo" />
+
+            <div className="navicons">
+                <img src={navicon} alt="navicon" />
+                <img src={navicon} alt="navicon" />
+                <img src={navicon} alt="navicon" />
+                <img src={navicon} alt="navicon" />
+            </div>
+
+            <img src={profileimg} alt="profimg" />
+
+
         </div>
         
         </>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Navbar from '../components/navbar';
 
 const Home = () => {
     return ( 
-        <h1>hi</h1>
+        <Navbar />
      );
 }
  
